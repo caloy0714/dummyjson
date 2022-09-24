6e405973e57054bd42d85866f2a6e5d7bc564cb2
caloy0714 <58654567+caloy0714@users.noreply.github.com>
2022-09-24 18:44:14 +0800

    Add files via upload

---
+++ delete-user.php
@@ -0,0 +1,49 @@
+<?php
+require "vendor/autoload.php";
+use GuzzleHttp\Client;
+
+$client = new Client([
+    'base_uri' => 'https://dummyjson.com/'
+    ]);
+$response = $client->delete('https://dummyjson.com/users/2');
+$code = $response->getStatusCode();
+$body = $response->getBody();
+$user = json_decode($body);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delete User</title>
+</head>
+<body>
+    <div class = "container">
+        <table class="table table-secondary">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Is Deleted?</th> 
+                    <th scope="col">Deleted On</th> 
+                    </tr>
+            </thead>
+        <tbody>
+            <tr>
+                <th scope="row" href="single-user.php"><?= $user->id ?></th>
+                <td><?=$user->firstName?></td>
+                <td><?=$user->lastName?></td>
+                <td><?=$user->email?></td>
+                <td><?=$user->isDeleted?></td>
+                <td><?=$user->deletedOn?></td>
+            </tr>
+        </tbody>
+</table>
