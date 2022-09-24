fc12d1186f51f101b1f57691b7423abc45fa0d9f
caloy0714 <58654567+caloy0714@users.noreply.github.com>
2022-09-24 18:18:58 +0800

    Delete delete.php

--- delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "vendor/autoload.php";
-use GuzzleHttp\Client;
-
-//$client = new \GuzzleHttp\Client(["base_uri" => "https://dummyjson.com/products/2"]);
-$response = $client->delete('https://dummyjson.com/products/2');
-
-var_dump($response);
